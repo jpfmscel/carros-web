f5009c9efae30b0a3fd76f943b09aba4a6d0073d
jpfmscel <jpfmscel@gmail.com>
2017-04-27 22:58:44 -0300

    UserResource

--- src/main/java/user/UserResource.java
+++ src/main/java/user/UserResource.java
@@ -1,12 +1,15 @@
 package user;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import users.entidades.User;
 import users.service.UserService;
 
 @Path(value = "/users")
@@ -20,4 +23,20 @@ public class UserResource {
 	public Response findAll() {
 		return Response.ok().entity(userService.findAll()).build();
 	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response insert() {
+		return Response.ok().entity("Insert ok").build();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(User user) {
+		userService.update(user);
+		return Response.ok().entity("Insert ok").build();
+	}
+	
 }
